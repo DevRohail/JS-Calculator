46aee13a590d25fb7a8405567b2c4975fc798fec
Rohail Ahmed <rohailahmed2010@gmail.com>
2022-03-22 19:42:52 +0000

    FINISHED!!
    After 3 weeks of continuous coding and a lot of troubleshooting I have finally completed it from head to toe. Please note, it may look a bit dodgy on some devices.

--- script.js
+++ script.js
@@ -55,16 +55,35 @@ class Calculator {
         this.operation = undefined
         this.previousOperand = ''
     }
-getDisplayNumber(number){
-    return number
-}
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+
+
+            integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        }
+        else {
+            return integerDisplay
+        }
+    }
 
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.getDisplayNumber (this.currentOperand)
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
         if (this.operation != null) {
             this.previousOperandTextElement.innerText =
-                `${this.getDisplayNumber (this.previousOperand)} ${this.operation}`
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
         }
     }
 }
